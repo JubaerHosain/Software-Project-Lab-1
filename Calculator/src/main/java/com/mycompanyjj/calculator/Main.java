c90fefef597b29524fb041eab0ac5cc020a81a96
Jubaer <jubaerhosain1119@gmail.com>
2021-08-25 00:33:02 +0600

    Added user interaction in Main.java

--- Calculator/src/main/java/com/mycompanyjj/calculator/Main.java
+++ Calculator/src/main/java/com/mycompanyjj/calculator/Main.java
@@ -16,25 +16,90 @@ public class Main {
     private Scanner scan;
     
     public Main() {
-       scan = new Scanner(System.in);
+        scan = new Scanner(System.in);
+    }
+    
+    private void basic() {
+        while(true) {
+            System.out.println("\t\t\t\t\tChosse A Option(Basic Mode):");
+            System.out.println("\t\t\t\t\tEnter An Arithmetic Expression To Calculate:");
+            System.out.println("\t\t\t\t\tEnter '0' For Go To Main Menu:");
+            
+            String input = scan.nextLine();
+            if(input.equals("0")) {
+                return;
+            }
+            else {
+                InfixEvaluation obj = new InfixEvaluation();
+                System.out.println("\t\t\t\t\t" + input + " = " + obj.evaluate(input) + '\n');
+            }
+        }
+    }
+    
+    private void programmer() {
+        while(true) {
+            System.out.println("\t\t\t\t\tChosse A Option(Programmer Mode):");
+            System.out.println("\t\t\t\t\tPress 1 For Number Base Conversion:");
+            System.out.println("\t\t\t\t\tPress 2 For Arithmetic Expression Conversion:");
+            System.out.println("\t\t\t\t\tPress 0 For Go To Main Menu:");
+            
+            int option = scan.nextInt();
+            if(option == 1) {
+                
+            } 
+            else if(option == 2) {
+                
+            }
+            else if(option == 0) {
+                return;
+            } 
+            else {
+                System.out.println("\n\t\t\t\t\tWrong Option!! Choose Correct One.\n");
+            }
+        }
     }
     
     public void start() {
-//        InfixEvaluation obj1 = new InfixEvaluation();
-//        System.out.println(obj1.evaluate("2(-5)+3+(-10/3)*3"));
-
-        int n = 3;
-        double[][] matrix = {{ 0, 2, 1, 4 },
-                            { 1, 1, 2, 6 },
-                            { 2, 1, 1, 7 }};
-        LinearEquationSolver obj2 = new LinearEquationSolver(matrix, n);
-        obj2.solveEquation();
+        System.out.println();
+        System.out.println("\t\t\t\t\t================Welcome To Calculator===============\n");
         
+        while(true) {
+            System.out.println("\t\t\t\t\tChosse A Option(Main Menu):");
+            System.out.println("\t\t\t\t\tPress 1 For Basic Mode: ");
+            System.out.println("\t\t\t\t\tPress 2 For Programmer Mode: ");
+            System.out.println("\t\t\t\t\tPress 3 For Linear Equation Solver mode: ");
+            System.out.println("\t\t\t\t\tPress 0 For Exit: ");
+            
+            Scanner scan = new Scanner(System.in);
+            int option = scan.nextInt();
+            
+            switch(option) {
+                case 1:
+                    basic();
+                    break;
+                    
+                case 2:
+                    programmer();
+                    break;
+                
+                case 3:
+                    break;
+                
+                case 0:
+                    System.exit(0);
+                    break;
+                
+                default:
+                    System.out.println("\n\t\t\t\t\tWrong Option!! Choose Correct One.\n");
+            }
+            
+        }
         
     }
    
     public static void main(String[] args) {
         Main obj = new Main();
         obj.start();
+        
     }
 }
